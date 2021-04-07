0d67c313c06df6ea579f6529e1b12f37e0aee392
Anonymous <none>
2021-04-06 19:56:55 -0700

    add chests, genes, manillo shop items; all objects necessary for first release

--- randomizer.py
+++ randomizer.py
@@ -1,9 +1,10 @@
 from randomtools.tablereader import (
-    TableObject, addresses)
+    TableObject, addresses, get_activated_patches, get_open_file)
 from randomtools.utils import (
     classproperty, cached_property, utilrandom as random)
 from randomtools.interface import (
-    run_interface, clean_and_write, finish_interface)
+    run_interface, clean_and_write, finish_interface,
+    get_activated_codes)
 from collections import Counter
 from sys import argv
 from traceback import format_exc
@@ -44,6 +45,7 @@ class ShopObject(TableObject):
         1: WeaponObject,
         2: ArmorObject,
         3: AccessoryObject,
+        4: KeyItemObject,
         }
 
     def __repr__(self):
@@ -72,7 +74,49 @@ class ShopObject(TableObject):
 
 class MasterSkillObject(TableObject): pass
 class MasterStatsObject(TableObject): pass
-class BaseStatsObject(NameMixin): pass
+class BaseStatsObject(NameMixin):
+    def cleanup(self):
+        if 'easymodo' in get_activated_codes():
+            self.accuracy = 100
+            self.base_accuracy = 100
+            self.current_hp = 999
+            self.max_hp = 999
+            self.base_max_hp = 999
+
+
+class ChestObject(TableObject):
+    def __repr__(self):
+        if self.item:
+            s = 'CHEST {0:0>2X} ({1:0>3}-{2:0>2x}): {3}'.format(
+                self.index, self.area_code, self.memory, self.item.name)
+        else:
+            assert self.item_type == 0xFF
+            zenny = '{0}Z'.format(self.item_index * 40)
+            s = 'CHEST {0:0>2X} ({1:0>3}-{2:0>2x}): {3}'.format(
+                self.index, self.area_code, self.memory, zenny)
+        return s
+
+    @property
+    def area_code(self):
+        filename = self.filename[-11:]
+        assert filename.startswith('AREA') and filename.endswith('.EMI')
+        return int(filename[-7:-4])
+
+    @property
+    def item(self):
+        if self.item_type in ShopObject.ITEM_TYPE_MAP:
+            obj = ShopObject.ITEM_TYPE_MAP[self.item_type]
+            return obj.get(self.item_index)
+        return None
+
+
+class GeneObject(TableObject):
+    def cleanup(self):
+        if self.gene_index == 0x21:
+            assert 'patch_flame_gene.txt' in get_activated_patches()
+            self.gene_index = 0
+        assert 0 <= self.gene_index <= 0x11
+
 
 class FormationObject(TableObject):
     def __repr__(self):
@@ -99,7 +143,41 @@ class FormationObject(TableObject):
         return [self.available_enemies[eid] if eid < 0xff else None
                 for eid in self.monster_indexes]
 
-class MonsterObject(NameMixin): pass
+    def cleanup(self):
+        if 'easymodo' in get_activated_codes():
+            self.appearance_rate = 0
+            if self.old_data['appearance_rate'] != 0:
+                self.monster_indexes = [0xff]*8
+
+
+class ManilloItemObject(TableObject):
+    def __repr__(self):
+        fishdesc = ', '.join(
+            '{0} x{1}'.format(fish.name, n) for (fish, n) in self.fishes)
+        s = 'TRADE {0:0>2X}: {1} ({2})'.format(
+            self.index, self.item.name, fishdesc)
+        return s.strip()
+
+    @property
+    def item(self):
+        obj = ShopObject.ITEM_TYPE_MAP[self.item_type]
+        return obj.get(self.item_index)
+
+    @property
+    def fishes(self):
+        fishes = []
+        for i, n in zip(self.fish_indexes, self.fish_quantities):
+            if i == 0xFF or n == 0:
+                continue
+            fish = ItemObject.get(0x38 + i)
+            fishes.append((fish, n))
+        return fishes
+
+
+class MonsterObject(NameMixin):
+    def cleanup(self):
+        if 'easymodo' in get_activated_codes():
+            self.hp = min(self.old_data['hp'], 1)
 
 
 if __name__ == '__main__':
@@ -110,7 +188,9 @@ if __name__ == '__main__':
         ALL_OBJECTS = [g for g in globals().values()
                        if isinstance(g, type) and issubclass(g, TableObject)
                        and g not in [TableObject]]
-        codes = {}
+        codes = {
+            'easymodo': ['easymodo'],
+            }
         run_interface(ALL_OBJECTS, snes=False, codes=codes,
                       custom_degree=True)
 
